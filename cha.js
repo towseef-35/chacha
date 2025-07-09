9783b0658bc6ef05b692be263e0fb0d8050343c7
towseef-35 <lonetowseef420@gmail.com>
2025-07-09 17:55:27 +0530

    js change

--- cha.js
+++ cha.js
@@ -3,3 +3,14 @@ let x=2,y=2;
 // let y=Math.floor(Math.random()*100);
 let z=Math.pow(x,y)
 console.table([x,y,z]);
+let email="lonetowseef420@gmail.com";
+console.log(`your email is : ${email}`);
+document.getElementById("p1").textContent=email;
+document.getElementById("btn")
+function lone(){
+    console.log("congratulations")
+}
+let z1 =Math.floor(Math.random()*100);
+console.log(z1)
+let input=window.prompt("enter your name")
+document.getElementById("p2").textContent=input;
