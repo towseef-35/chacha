8d9eefd20a2070090a2f0f9d0b55b9c522e1a6d6
towseef-35 <lonetowseef420@gmail.com>
2025-07-09 23:50:00 +0530

    counter program

--- cha.js
+++ cha.js
@@ -1,16 +1,23 @@
-console.log("hello")
-let x=2,y=2;
-// let y=Math.floor(Math.random()*100);
-let z=Math.pow(x,y)
-console.table([x,y,z]);
-let email="lonetowseef420@gmail.com";
-console.log(`your email is : ${email}`);
-document.getElementById("p1").textContent=email;
-document.getElementById("btn")
-function lone(){
-    console.log("congratulations")
-}
-let z1 =Math.floor(Math.random()*100);
-console.log(z1)
-let input=window.prompt("enter your name")
-document.getElementById("p2").textContent=input;
+// 1:simple counter program 
+// const btndec=document.getElementById("btndec");
+
+// const btninc=document.getElementById("btninc");
+
+// const btnres=document.getElementById("btnres");
+
+// const countlabel=document.getElementById("countlabel");
+// let count=0;
+// btninc.onclick=function(){
+//     count ++;
+//     countlabel.textContent=count;
+// }
+// btndec.onclick=function(){
+//     count --;
+//     countlabel.textContent=count;
+// }
+// btnres.onclick=function(){
+//     count =0;
+//     countlabel.textContent=count;
+// }
+
+
